b0d368b4404c500e8f11b0115fbf0f99ec0b647f
notabhishek22 <lit2021004@iiitl.ac.in>
2024-01-17 15:07:04 +0530

    added subtle changes

--- app/components/companySignIn.js
+++ app/components/companySignIn.js
@@ -8,15 +8,15 @@ export default function CompanySignIn() {
 
   const handleSignIn = async () => {
     const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-      })
+      email: email,
+      password: password,
+    });
     if (error) {
       console.log(supabase);
-      console.error("Error signing in:", error.message);
+      alert(error.message);
     } else {
       console.log(supabase);
-      window.location="http://localhost:3000/company"
+      window.location = "http://localhost:3000/company";
       console.log("Signed in successfully:", data);
     }
   };
@@ -25,18 +25,22 @@ export default function CompanySignIn() {
     <div>
       <h2>Sign In</h2>
       <label>Email:</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label>Password:</label>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignIn}>Sign In</button>
+      <form action={handleSignIn}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">
+          Sign In
+        </button>
+      </form>
     </div>
   );
 }
